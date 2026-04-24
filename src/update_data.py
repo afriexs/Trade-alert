4f20dc7d6255317b03aed18e66ba981afdff01ae
afriexs <afriex20@gmail.com>
2026-04-24 12:53:36 +0100

    Update update_data.py

--- src/update_data.py
+++ src/update_data.py
@@ -1,6 +1,6 @@
 import requests, json, time
+from rate_history import update_history
 
-# ---------------- SAVE HELPER ----------------
 def save_json(filename, data):
     with open(filename, "w") as f:
         json.dump({
@@ -10,34 +10,30 @@ def save_json(filename, data):
         }, f)
 
 
-# ---------------- FOREX ----------------
 def update_forex():
     url = "https://latest.currency-api.pages.dev/v1/currencies/usd.json"
     res = requests.get(url).json()
 
-    # extract only usd rates
     forex_data = {}
-
     for k, v in res.get("usd", {}).items():
         forex_data[k.upper()] = v
 
     save_json("data/forex.json", forex_data)
+    update_history("data/forex.json", "forex")
 
 
-# ---------------- CRYPTO ----------------
 def update_crypto():
     url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
     res = requests.get(url).json()
 
     crypto_data = {}
-
-    for coin in res[:100]:  # top 100
+    for coin in res[:100]:
         symbol = coin["symbol"].upper()
-        price = coin["current_price"]
-        crypto_data[symbol] = price
+        crypto_data[symbol] = coin["current_price"]
 
     save_json("data/crypto.json", crypto_data)
+    update_history("data/crypto.json", "crypto")
 
-# ---------------- RUN ----------------
+
 update_forex()
 update_crypto()
