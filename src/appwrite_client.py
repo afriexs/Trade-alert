d352f85667370a113a8fd339413b76cb6c08624e
afriexs <afriex20@gmail.com>
2026-05-01 08:09:05 +0100

    Update appwrite_client.py

--- src/appwrite_client.py
+++ src/appwrite_client.py
@@ -1,5 +1,6 @@
 from appwrite.client import Client
 from appwrite.services.databases import Databases
+from appwrite.services.tablesDB import TablesDB
 import config
 
 client = Client()
@@ -8,6 +9,7 @@ client.set_project(config.APPWRITE_PROJECT)
 client.set_key(config.APPWRITE_API_KEY)
 
 db = Databases(client)
+tables = TablesDB(client)
 
 def get_users():
     return db.list_documents(
